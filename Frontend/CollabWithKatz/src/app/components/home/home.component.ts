10c7870d5e573c40ae07aa99c73e356a25f5a394
Vignesh <vignesh000129@gmail.com>
2023-08-29 23:30:51 +0530

    Added video and video elements in html in home page

--- Frontend/CollabWithKatz/src/app/components/home/home.component.ts
+++ Frontend/CollabWithKatz/src/app/components/home/home.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent {// Replace with actual video path
+  videoSources = [
+    '../../../assets/cat_demo.mp4',
+    '../../../assets/cat_demo.mp4',
+    '../../../assets/cat_demo.mp4'
+    // Add more video sources as needed
+  ];
+  playVideo(video: HTMLVideoElement) {
+    video.play();
+  }
 
+  pauseVideo(video: HTMLVideoElement) {
+    video.pause();
+    video.currentTime = 0;
+  }
+  
 }
