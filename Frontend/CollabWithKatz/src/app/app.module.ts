d5e0669dc622c0105066c4693fb92ae96c361452
Vignesh <vignesh000129@gmail.com>
2023-08-30 21:08:04 +0530

    signup page template created

--- Frontend/CollabWithKatz/src/app/app.module.ts
+++ Frontend/CollabWithKatz/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FileuploadComponent } from './components/fileupload/fileupload.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    FileuploadComponent
+    FileuploadComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
