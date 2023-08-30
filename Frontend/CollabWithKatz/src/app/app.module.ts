1ea993f028d93e32f9def38561a9fec0bf1c3cb1
Vignesh <vignesh000129@gmail.com>
2023-08-30 20:27:53 +0530

    Added fileUpload template

--- Frontend/CollabWithKatz/src/app/app.module.ts
+++ Frontend/CollabWithKatz/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FileuploadComponent } from './components/fileupload/fileupload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    FileuploadComponent
   ],
   imports: [
     BrowserModule,
