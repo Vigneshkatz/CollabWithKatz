4dbff19d3297ad717fa5f4221f0d1dae194d3181
Vignesh <vignesh000129@gmail.com>
2023-09-09 09:23:56 +0530

    cta style changed

--- Frontend/CollabWithKatz/src/app/components/landingpage/landingpage.component.ts
+++ Frontend/CollabWithKatz/src/app/components/landingpage/landingpage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landingpage',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./landingpage.component.css']
 })
 export class LandingpageComponent {
+  constructor(private router: Router) {
+
+  }
+  onLearnMoreClick() {
+    this.router.navigate(['/signup']);
+  }
 
 }
