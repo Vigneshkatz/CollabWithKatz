ab08ceaccea77be78eb82335246152561d96f330
shyam kachhadiya <shyam1ss15@gmail.com>
2023-09-12 16:16:45 +0000

    fix

--- Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/CollabWithKatzApplication.java
+++ Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/CollabWithKatzApplication.java
@@ -3,8 +3,6 @@ package com.katziio.collabwithkatz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileNotFoundException;
-
 @SpringBootApplication
 public class CollabWithKatzApplication {
     public static void main(String[] args)  {
