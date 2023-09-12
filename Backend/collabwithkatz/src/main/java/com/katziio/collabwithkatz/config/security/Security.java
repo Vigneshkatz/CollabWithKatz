d06734651782678d7b2c6f4a41d6adc851d383f2
shyam kachhadiya <shyam1ss15@gmail.com>
2023-09-12 16:16:45 +0000

    fix

--- Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/config/security/Security.java
+++ Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/config/security/Security.java
@@ -29,43 +29,50 @@ package com.katziio.collabwithkatz.config.security;
 //}
 
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class Security{
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-////                .csrf()
-////                .disable()
-//                .authorizeRequests(configurer ->
-//                        configurer
-//                                .requestMatchers("/").permitAll()
-//                                .requestMatchers("/posts/create/post").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
-//                                .requestMatchers("/posts/filter").permitAll()
-//                                .requestMatchers("/posts/update/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
-//                                .requestMatchers("/posts/delete/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
-//                                .requestMatchers("/posts/drafts/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
-//                                .requestMatchers("/drafts/read/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
-//                                .requestMatchers("/comment/create").permitAll()
-//                                .requestMatchers("/comment/delete/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
-//                                .requestMatchers("/comment/update/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
-//                                .requestMatchers("/error").permitAll()
-//                ).
-//                .formLogin()
-//                .and()
-//
-//                .logout(LogoutConfigurer::permitAll)
-//                .build();
-//    }
-//    @Bean
-//    public PasswordEncoder getPasswordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
-//}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Security{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+//                .csrf()
+//                .disable()
+                .authorizeRequests(configurer ->
+                        configurer
+                                /*.requestMatchers("/").permitAll()
+                                .requestMatchers("/posts/create/post").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
+                                .requestMatchers("/posts/filter").permitAll()
+                                .requestMatchers("/posts/update/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
+                                .requestMatchers("/posts/delete/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
+                                .requestMatchers("/posts/drafts/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
+                                .requestMatchers("/drafts/read/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
+                                .requestMatchers("/comment/create").permitAll()
+                                .requestMatchers("/comment/delete/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
+                                .requestMatchers("/comment/update/**").hasAnyAuthority(Constants.ADMIN, Constants.AUTHOR)
+                                .requestMatchers("/error").permitAll()*/
+                                .requestMatchers("/public/**").permitAll() // Publicly accessible URLs
+                                .requestMatchers("/admin/**").hasRole("ADMIN") // Requires ADMIN role
+                                .requestMatchers("/user/**").hasRole("USER") // Requires USER role
+                                //.requestMatchers("").authenticated() // All other URLs require authentication
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(LogoutConfigurer::permitAll)
+                .build();
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
