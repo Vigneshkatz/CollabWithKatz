a9f55e0fae188170e529ee69430e8496253e2222
Vignesh <iamkatz08@gmail.com>
2023-09-26 08:31:33 +0530

    Creator Login bug fix

--- Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/repository/creator/CreatorRepository.java
+++ Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/repository/creator/CreatorRepository.java
@@ -13,7 +13,7 @@ public interface CreatorRepository extends JpaRepository<Creator,Long> {
     @Query("SELECT new com.katziio.collabwithkatz.dto.creator.CreatorDTO(c) FROM Creator c ORDER BY c.id DESC LIMIT 1")
     CreatorDTO findLatestCreator();
 
-    @Query("SELECT new com.katziio.collabwithkatz.dto.creator.CreatorDTO(c) FROM Editor c WHERE c.email =:userEmail AND c.password =:userPassword")
+    @Query("SELECT new com.katziio.collabwithkatz.dto.creator.CreatorDTO(c) FROM Creator c WHERE c.email =:userEmail AND c.password =:userPassword")
     CreatorDTO isValidLogin(@Param("userEmail") String userEmail, @Param("userPassword") String userPassword);
 
 }
