40fe3b5aee732b3b3c4f30d7745b668982fd3d62
Vignesh <vignesh000129@gmail.com>
2023-10-02 08:16:39 +0530

    Added Unique to email and username

--- Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/entity/creator/Creator.java
+++ Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/entity/creator/Creator.java
@@ -16,7 +16,9 @@ public class Creator {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
 
--- Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/entity/editor/Editor.java
+++ Backend/collabwithkatz/src/main/java/com/katziio/collabwithkatz/entity/editor/Editor.java
@@ -19,8 +19,10 @@ public class Editor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String name;
     private String about;
+    @Column(unique = true)
     private String email;
     private String password;
     private int age;
